d2aebd18affbfb306f9ea167acf1d194b579f1e8
Maksym Martynenko <maksym_martynenko@epam.com>
2025-10-01 13:52:00 +0000

    feat: change products api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-	products: "https://v9js754xud.execute-api.eu-central-1.amazonaws.com/prod/",
+	products: "https://p7aervwldc.execute-api.eu-central-1.amazonaws.com/prod",
 	order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 	import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 	bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
