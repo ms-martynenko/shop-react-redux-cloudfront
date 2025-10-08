f70a45d8fc69fe03a126e97a38d86ca3e2f6bfdf
Maksym Martynenko <maksym_martynenko@epam.com>
2025-10-08 13:58:11 +0000

    feat: unlock importing

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,60 +1,65 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import axios from "axios";
 
 type CSVFileImportProps = {
-  url: string;
-  title: string;
+	url: string;
+	title: string;
 };
 
 export default function CSVFileImport({ url, title }: CSVFileImportProps) {
-  const [file, setFile] = React.useState<File>();
+	const [file, setFile] = React.useState<File>();
 
-  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files;
-    if (files && files.length > 0) {
-      const file = files[0];
-      setFile(file);
-    }
-  };
+	const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const files = e.target.files;
+		if (files && files.length > 0) {
+			const file = files[0];
+			setFile(file);
+		}
+	};
 
-  const removeFile = () => {
-    setFile(undefined);
-  };
+	const removeFile = () => {
+		setFile(undefined);
+	};
 
-  const uploadFile = async () => {
-    console.log("uploadFile to", url);
+	const uploadFile = async (file: File) => {
+		console.log("uploadFile to", url);
 
-    // Get the presigned URL
-    // const response = await axios({
-    //   method: "GET",
-    //   url,
-    //   params: {
-    //     name: encodeURIComponent(file.name),
-    //   },
-    // });
-    // console.log("File to upload: ", file.name);
-    // console.log("Uploading to: ", response.data);
-    // const result = await fetch(response.data, {
-    //   method: "PUT",
-    //   body: file,
-    // });
-    // console.log("Result: ", result);
-    // setFile("");
-  };
-  return (
-    <Box>
-      <Typography variant="h6" gutterBottom>
-        {title}
-      </Typography>
-      {!file ? (
-        <input type="file" onChange={onFileChange} />
-      ) : (
-        <div>
-          <button onClick={removeFile}>Remove file</button>
-          <button onClick={uploadFile}>Upload file</button>
-        </div>
-      )}
-    </Box>
-  );
+		const response = await axios({
+			method: "GET",
+			url,
+			params: {
+				name: encodeURIComponent(file.name),
+			},
+		});
+
+		console.log("File to upload: ", file.name);
+		console.log("Uploading to: ", response.data);
+
+		const result = await fetch(response.data, {
+			method: "PUT",
+			body: file,
+		});
+
+		console.log("Result: ", result);
+
+		removeFile();
+	};
+
+	return (
+		<Box>
+			<Typography variant="h6" gutterBottom>
+				{title}
+			</Typography>
+			{!file ? (
+				<input type="file" onChange={onFileChange} />
+			) : (
+				<div>
+					<button onClick={removeFile}>Remove file</button>
+					<button onClick={() => uploadFile(file)}>Upload file</button>
+				</div>
+			)}
+		</Box>
+	);
 }
